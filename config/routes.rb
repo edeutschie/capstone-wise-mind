40463c3a3db623481954393b7d836e6a1061cdc2
Elizabeth Deutsch <elizabeth.deutsch@att.net>
2017-07-07 10:04:19 -0700

    index routes for quotes and users

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'quotes', to: 'quotes#index', as: 'quotes'
+
+  get 'users', to: 'users#index', as: 'users'
+
 end
