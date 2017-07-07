18efb8240c58464a1ead527e4633ab4280999a8c
Elizabeth Deutsch <elizabeth.deutsch@att.net>
2017-07-06 17:06:33 -0700

    added validations to quote and user models

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,20 @@
 class Quote < ApplicationRecord
+  # has_and_belongs_to_many :users
+  has_many :quotesusers
+  has_many :users, through: :quotesusers
+
+  belongs_to :user
+
+  validates :text, presence: true, uniqueness: true
+  validates :author, presence: true
+  validates :theme, presence: true, inclusion: {
+    in: [ "Motivational", "Creativity", "My Quotes", "Shuffle"]
+  }
+  validates :public, presence: true, inclusion: { in: [ true, false ] }
+
+  # def rando
+  #   return 5 + 5
+  # end
+  #validates :user_id, presence: true, numericality: {only_integer: true, greater_than: 0}, on: :create
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,19 @@
 class User < ApplicationRecord
+  # has_and_belongs_to_many :quotes
+  has_many :quotesusers
+  has_many :quotes, through: :quotesusers
+
+  has_many :submitted_quotes, class_name: :Quote, foreign_key: "user_id"
+
+  before_validation :clean_up_phone_num
+
+  validates :phone_num, presence: true
+
+  def clean_up_phone_num
+    return self.phone_num.gsub(/[^0-9]/, '')
+  end
+
+  # def rando
+  #   return 2 + 2
+  # end
 end
