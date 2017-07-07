e944ed06a990a2a9a37e5a6ae0d88153e9b5d2c6
Elizabeth Deutsch <elizabeth.deutsch@att.net>
2017-07-07 11:50:45 -0700

    post routes for users and quotes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'quotes', to: 'quotes#index', as: 'quotes'
   get 'quotes/:id', to:'quotes#show', as: 'quote'
+  post 'quotes', to: 'quotes#create'
 
   get 'users', to: 'users#index', as: 'users'
   get 'users/:id', to: 'users#show', as: 'user'
+  post 'users', to: 'users#create'
 
 end
