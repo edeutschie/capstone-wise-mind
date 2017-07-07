77ca96525d655759126b96735f927585a67db072
Elizabeth Deutsch <elizabeth.deutsch@att.net>
2017-07-07 10:55:18 -0700

    user and quote show routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'quotes', to: 'quotes#index', as: 'quotes'
+  get 'quotes/:id', to:'quotes#show', as: 'quote'
 
   get 'users', to: 'users#index', as: 'users'
+  get 'users/:id', to: 'users#show', as: 'user'
 
 end
