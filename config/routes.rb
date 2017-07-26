8c54d6b07055f039d27de8ddc1bf8abd12e37bc6
Elizabeth Deutsch <elizabeth.deutsch@att.net>
2017-07-26 16:36:59 -0700

    took out back slashes in github routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/auth/github', to: 'authentication#github', format: false
+  # root :to => '#index'
 
-  get '/auth/github/callback', to: 'authentication#github', format: false
+  get 'auth/github', to: 'authentication#github', format: false
+
+  get 'auth/github/callback', to: 'authentication#github', format: false
 
   post 'notifications/notify' => 'notifications#notify'
 
