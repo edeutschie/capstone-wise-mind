3302b077351392e6713d76d0267e5bf88ce60d23
Elizabeth Deutsch <elizabeth.deutsch@att.net>
2017-07-26 23:31:23 -0700

    reversed last change to show method, users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,8 @@ class UsersController < ApplicationController
 
   def show
 
-    login = TokiToki.decode(params[:token]).first["login"]
-    # login = TokiToki.decode(params[:token]).first["sub"]
+    # login = TokiToki.decode(params[:token]).first["login"]
+    login = TokiToki.decode(params[:token]).first["sub"]
     # fix above with error checking
     user = User.find_by(login: login)
     if user
