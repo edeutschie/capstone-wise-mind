db463d2f6e2a4696eb1196d4fdb6c86156dde411
Elizabeth Deutsch <elizabeth.deutsch@att.net>
2017-07-26 19:09:34 -0700

    commented out root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # root :to => '#index'
-
   get 'auth/github', to: 'authentication#github', format: false
 
   get 'auth/github/callback', to: 'authentication#github', format: false
